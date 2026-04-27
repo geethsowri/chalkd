2e840a22f4f7ffb4cd2b3b4478b5c6f3e5a13099
Sowri <geethsowri.n@inncircles.com>
2026-04-27 22:50:35 +0530

    feat(pages): Next.js app shell, index, room route, custom document
    _app sets up Recoil + global styles + toast + ModalManager; _document
    defines the HTML shell; index renders Home; [roomId] renders Room.

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Main, NextScript, Head } from "next/document";
+
+const document = () => (
+  <Html>
+    <Head>
+    </Head>
+    <body>
+      <div id="portal"></div>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default document;
