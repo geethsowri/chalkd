d406d06b98725e4d57246b4c9461f6b561b78fde
Sowri <geethsowri.n@inncircles.com>
2026-06-27 10:46:42 +0530

    fix(rgba.ts): update note

--- common/lib/rgba.ts
+++ common/lib/rgba.ts
@@ -3,3 +3,4 @@ import { RgbaColor } from "react-colorful";
 
 export const getStringFromRgba = (rgba: RgbaColor) =>
   `rgba(${rgba.r}, ${rgba.g}, ${rgba.b}, ${rgba.a})`;
+// note: update 17828389076141
